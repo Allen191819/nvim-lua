10b38028a48aa3e248d26cbed2767ac99557f91a
Allen1918 <2020302191891@whu.edu.cn>
2021-09-27 11:02:36 +0800

    update 20210926

--- lua/lsp/lua-ls.lua
+++ lua/lsp/lua-ls.lua
@@ -1,22 +1,10 @@
-local sumneko_root_path = "/home/allen/.config/nvim/language-servers/lua-language-server"
-local sumneko_binary = "/home/allen/.config/nvim/language-servers/lua-language-server/bin/Linux/lua-language-server"
-require'lspconfig'.sumneko_lua.setup {
-    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
-    filetypes = { "lua" },
+require'lspconfig'.lua.setup {
     settings = {
         Lua = {
-            runtime = {
-                version = 'LuaJIT',
-                path = vim.split(package.path, ';')
-            },
-            diagnostics = {
-                globals = {'vim'}
-            },
             workspace = {
-                library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,[vim.fn.expand('${3rd}/love2d/library')]=true},
                 checkThirdParty = false,
                 maxPreload = 1500,
-                preloadFileSize = 500
+                preloadFileSize = 300
             }
         }
     }
