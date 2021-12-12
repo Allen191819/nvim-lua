e94439433f9be1e0add4028122f3889d8f9ded4e
Allen1918 <2020302191891@whu.edu.cn>
2021-12-13 00:19:53 +0800

    nvim 0.6 config update

--- lua/function.lua
+++ lua/function.lua
@@ -1,7 +1,44 @@
-vim.api.nvim_set_keymap("n", "R", ":call CompileRunGcc()<CR>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "Rh", ":call CompileRunGccH()<CR>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "Rf", ":call CompileRunGccF()<CR>", {silent = true, noremap = true})
 vim.cmd(
     [[
-func! CompileRunGcc()
+func! CompileRunGccH()
+	exec "w"
+	if &filetype == 'c'
+		exec "!gcc % -o %< -g"
+		:FloatermNew --position=bottom --wintype=split --height=0.35 time ./%<
+	elseif &filetype == 'cpp'
+		exec "!g++ -std=c++11 % -Wall -o %< -g"
+		:FloatermNew --position=bottom --wintype=split --height=0.35 time ./%<
+	elseif &filetype == 'java'
+		:FloatermNew --position=bottom --wintype=split --height=0.35 javac % && java %<
+    elseif &filetype == 'lua'
+		:FloatermNew --position=bottom --wintype=split --height=0.35 lua %
+	elseif &filetype == 'sh'
+		:FloatermNew --position=bottom --wintype=split --height=0.35 bash %
+	elseif &filetype == 'python'
+		:FloatermNew --position=bottom --wintype=split --height=0.35 python3 %
+	elseif &filetype == 'markdown'
+		exec "MarkdownPreview"
+	elseif &filetype == 'tex'
+        silent! exec "TexKill"
+        silent! exec "TexCompile"
+	elseif &filetype == 'html'
+		silent! exec "!".g:mkdp_browser." % &"
+	elseif &filetype == 'javascript'
+		:FloatermNew --position=bottom --wintype=split --height=0.35 export DEBUG="INFO,ERROR,WARNING"; node --trace-warnings .
+	elseif &filetype == 'go'
+		:FloatermNew --position=bottom --wintype=split --height=0.35 go run %
+	elseif &filetype == 'csv'
+        silent! exec "lua require('nvim-preview-csv').preview()"
+	endif
+endfunc
+]]
+)
+
+vim.cmd(
+    [[
+func! CompileRunGccF()
 	exec "w"
 	if &filetype == 'c'
 		exec "!gcc % -o %< -g"
--- lua/nv-markdown/init.lua
+++ lua/nv-markdown/init.lua
@@ -6,7 +6,7 @@ vim.g.mkdp_browser = 'surf'
 vim.g.mkdp_highlight_css = ''
 vim.g.mkdp_page_title = '「${name}」'
 vim.g.mkdp_filetypes = {'markdown'}
-vim.g.mkdp_preview_options = {hide_yaml_meta = 1, disable_filename = 1, theme = 'light' }
+vim.g.mkdp_preview_options = {hide_yaml_meta = 1, disable_filename = 1, theme = 'dark' }
 vim.g.mkdp_markdown_css = '/home/allen/.config/nvim/color/markdown.css'
 
 vim.g.vmt_auto_update_on_save = 1
